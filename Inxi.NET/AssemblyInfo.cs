a53d58d2d1f7cfd4542c0a3805496042a4aa2f18
SpReeDateD <webmaster@born2skate.de>
2023-01-26 23:30:58 +0100

    strong name assembly signing

---
+++ Inxi.NET/AssemblyInfo.cs
@@ -0,0 +1,19 @@
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// In SDK-style projects such as this one, several assembly attributes that were historically
+// defined in this file are now automatically added during build and populated with
+// values defined in project properties. For details of which attributes are included
+// and how to customise this process see: https://aka.ms/assembly-info-properties
+
+
+// Setting ComVisible to false makes the types in this assembly not visible to COM
+// components.  If you need to access a type in this assembly from COM, set the ComVisible
+// attribute to true on that type.
+
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM.
+
+[assembly: Guid("e169bf33-d904-4aa9-baaa-fbe46ffd683b")]
+[assembly: InternalsVisibleTo("Inxi.NET.Tests, PublicKey=0024000004800000940000000602000000240000525341310004000001000100d985f45307959cb79f776f25c3f0650e7c28691156a6f949f450b96cd634a438a95a935616c7c507b7bd005d3c442213400f62fe189cd77cf47d9a1a92b9280afb59205ca52c464d457e193f8d4b16eb1c6e5c1e33e05ea53f2143e3c8d07cb31f1a4361001aa171fef997bf78e743a7a58d2de807f4d99a9e176b78bd1bf2b4")]
