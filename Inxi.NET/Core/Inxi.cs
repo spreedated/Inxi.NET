9f262bf32d42ce388451a4c8071a284970b55cfd
SpReeDateD <webmaster@born2skate.de>
2023-01-26 22:57:27 +0100

    refactored inxi.cs

--- Inxi.NET/Core/Inxi.cs
+++ Inxi.NET/Core/Inxi.cs
@@ -19,6 +19,7 @@
 using System;
 using System.IO;
 using System.Linq;
+using System.Threading.Tasks;
 using static System.Reflection.Assembly;
 
 namespace InxiFrontend
@@ -32,77 +33,120 @@ namespace InxiFrontend
         /// <summary>
         /// Hardware information
         /// </summary>
-        public readonly HardwareInfo Hardware;
+        public HardwareInfo Hardware;
 
+        public InxiHardwareType SelectedHardwareTypes { get; set; }
+        public string InxiPath { get; private set; } = "/usr/bin/inxi";
+        public string CpanelJsonXsPath { get; private set; } = "/usr/bin/cpanel_json_xs";
+        public string JsonXsPath { get; private set; } = "/usr/bin/json_xs";
+
+#region Constructor
         /// <summary>
         /// Intializes the new instance of Inxi class and parses hardware
         /// </summary>
-        public Inxi() : this("/usr/bin/inxi", "/usr/bin/cpanel_json_xs", "/usr/bin/json_xs", (InxiHardwareType)Enum.GetValues(typeof(InxiHardwareType)).Cast<int>().Sum())
+        public Inxi()
         {
+            foreach (InxiHardwareType h in Enum.GetValues(typeof(InxiHardwareType)).Cast<InxiHardwareType>())
+            {
+                this.SelectedHardwareTypes |= h;
+            }
         }
 
         /// <summary>
         /// Intializes the new instance of Inxi class and parses hardware
         /// </summary>
         /// <param name="HardwareTypes">Hardware types to parse</param>
-        public Inxi(InxiHardwareType HardwareTypes) : this("/usr/bin/inxi", "/usr/bin/cpanel_json_xs", "/usr/bin/json_xs", HardwareTypes)
+        public Inxi(InxiHardwareType HardwareTypes)
         {
+            this.SelectedHardwareTypes = HardwareTypes;
+        }
+#endregion
+
+        /// <summary>
+        /// Async version of the capturing process
+        /// </summary>
+        /// <returns></returns>
+        public Task RetrieveInformationAsync()
+        {
+            return Task.Factory.StartNew(() =>
+            {
+                this.RetrieveInformation();
+            });
         }
 
         /// <summary>
-        /// Initializes the new instance of Inxi class with specified path and parses hardware
+        /// Runs the capture process
         /// </summary>
-        /// <param name="InxiPath">Path to Inxi executable. It's usually /usr/bin/inxi. Ignored in Windows.</param>
-        /// <param name="CpanelJsonXsPath">Path to CPanelJsonXS executable. It's usually /usr/bin/cpanel_json_xs. Ignored in Windows.</param>
-        /// <param name="JsonXsPath">Path to JsonXS executable. It's usually /usr/bin/json_xs. Ignored in Windows.</param>
-        /// <param name="HardwareTypes">Hardware types to parse</param>
-        public Inxi(string InxiPath, string CpanelJsonXsPath, string JsonXsPath, InxiHardwareType HardwareTypes)
+        /// <exception cref="InvalidOperationException">Be sure to have Inxi installed</exception>
+        public void RetrieveInformation()
         {
             string FrontendVersion = GetExecutingAssembly().GetName().Version.ToString();
+
             if (InxiInternalUtils.IsUnix())
             {
-                InxiTrace.Debug("Inxi.NET {0} running on Unix.", FrontendVersion);
-                InxiTrace.Debug("Inxi parse flags: {0}", HardwareTypes);
-
-                // Check to see if we're on macOS or on regular Unix
-                if (InxiInternalUtils.IsMacOS())
-                {
-                    // Use System Profiler to get hardware information
-                    InxiTrace.Debug("Type: macOS");
-                    Hardware = new HardwareInfo(InxiPath, HardwareTypes);
-                }
-                else
-                {
-                    // Use Inxi to get hardware information
-                    InxiTrace.Debug("Type: Unix");
-                    InxiTrace.Debug("Looking for Inxi executable at {0}...", InxiPath);
-                    if (File.Exists(InxiPath))
-                    {
-                        InxiTrace.Debug("Looking for Json XS perl module binary at {0} or {1}...", CpanelJsonXsPath, JsonXsPath);
-                        if (File.Exists(CpanelJsonXsPath) | File.Exists(JsonXsPath))
-                        {
-                            InxiTrace.Debug("Found Json XS perl module!");
-                            Hardware = new HardwareInfo(InxiPath, HardwareTypes);
-                        }
-                        else
-                        {
-                            InxiTrace.Debug("Json XS perl module is not installed.");
-                            throw new InvalidOperationException("You must have libcpanel-json-xs-perl or libjson-xs-perl installed. (Could not find \"" + CpanelJsonXsPath + "\" or \"" + JsonXsPath + "\".)");
-                        }
-                    }
-                    else
-                    {
-                        InxiTrace.Debug("Inxi is not installed.");
-                        throw new InvalidOperationException("You must have Inxi installed. (Could not find \"" + InxiPath + "\".)");
-                    }
-                }
+                this.UnixRun(FrontendVersion);
+                return;
             }
-            else
-            {
-                InxiTrace.Debug("Inxi.NET {0} running on Windows.", FrontendVersion);
-                Hardware = new HardwareInfo(InxiPath, HardwareTypes);
-            }
+            this.WindowsRun(FrontendVersion);
         }
 
+        public void SetPaths(string inxiPath, string jsonXsPath, string cpanelJsonXsPath)
+        {
+            this.InxiPath = inxiPath;
+            this.JsonXsPath = jsonXsPath;
+            this.CpanelJsonXsPath = cpanelJsonXsPath;
+        }
+
+        private void WindowsRun(string frontendVersion)
+        {
+            InxiTrace.Debug("Inxi.NET {0} running on Windows.", frontendVersion);
+            this.Hardware = new HardwareInfo(InxiPath, this.SelectedHardwareTypes);
+        }
+
+        private void UnixRun(string frontendVersion)
+        {
+            InxiTrace.Debug("Inxi.NET {0} running on Unix.", frontendVersion);
+            InxiTrace.Debug("Inxi parse flags: {0}", this.SelectedHardwareTypes);
+
+            bool isMac = false;
+            // Check to see if we're on macOS or on regular Unix
+            if (InxiInternalUtils.IsMacOS())
+            {
+                // Use System Profiler to get hardware information
+                InxiTrace.Debug("Type: macOS");
+                isMac = true;
+            }
+
+            // Use Inxi to get hardware information
+            InxiTrace.Debug("Type: Unix");
+            InxiTrace.Debug("Looking for Inxi executable at {0}...", this.InxiPath);
+            if (!isMac && this.IsInxiInstalled())
+            {
+                InxiTrace.Debug("Found Json XS perl module!");
+            }
+
+            this.Hardware = new HardwareInfo(this.InxiPath, this.SelectedHardwareTypes);
+        }
+
+        private bool IsInxiInstalled()
+        {
+            InxiTrace.Debug("Looking for Inxi executable at {0}...", this.InxiPath);
+
+            if (File.Exists(this.InxiPath))
+            {
+                InxiTrace.Debug("Looking for Json XS perl module binary at {0} or {1}...", this.CpanelJsonXsPath, this.JsonXsPath);
+
+                if (File.Exists(this.CpanelJsonXsPath) | File.Exists(this.JsonXsPath))
+                {
+                    return true;
+                }
+
+                InxiTrace.Debug("Json XS perl module is not installed.");
+                throw new InvalidOperationException("You must have libcpanel-json-xs-perl or libjson-xs-perl installed. (Could not find \"" + this.CpanelJsonXsPath + "\" or \"" + this.JsonXsPath + "\".)");
+            }
+
+            InxiTrace.Debug("Inxi is not installed.");
+            throw new InvalidOperationException("You must have Inxi installed. (Could not find \"" + this.InxiPath + "\".)");
+        }
     }
 }
